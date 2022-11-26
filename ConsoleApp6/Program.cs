73b2346351ed9e05fb0129df866900b7951ba390
Daniel Toth <78477459+danieltoth-sys@users.noreply.github.com>
2022-11-26 16:01:24 +0100

    Upload

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace ConsoleApp6
+{
+    internal class Program
+    {        
+        static void Main(string[] args)
+        {
+            //Dátumkezelés alapfok
+            DateTime dateTime= DateTime.Now;
+
+            int day = dateTime.Day;
+            //Hibakezelés
+            try
+            {
+                if (day > 21)
+                {
+                    Console.WriteLine("Nagyobb");
+                }
+                else
+                {
+                    throw new Exception();
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Kisebb");
+            }
+                     
+            Console.WriteLine(dateTime.ToString());
+            Console.WriteLine(day.ToString());     
+            
+            //Sok if helyett ez inkább :)
+            switch (day)
+            {
+                case 20:
+                    Console.WriteLine("20");
+                    break;
+                case 26:
+                    Console.WriteLine("26!");
+                    break;
+                default:
+                    break;
+            }
+            int a = 10;
+            int b = 20;
+            Console.WriteLine(Tutorial.Fizetendo(a,b));
+        }
+    }
+
+    public class Tutorial
+    {
+        public static double Fizetendo(double adosav, double alap)
+        {
+            double visszateresi = adosav * alap;
+            return visszateresi;
+        }
+    }
+}
---
+++ TesztForm/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace TesztForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1127, 639);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(211, 78);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Gombocska";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 30);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Hello world";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(57, 89);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(283, 29);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1350, 729);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label label1;
+        private TextBox textBox1;
+    }
+}
